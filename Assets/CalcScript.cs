ff316046e6e0a2080808405098bb8de8ba70c7cd
Χρήστος Ταφαρλής <xristos.p.tafarlis@gmail.com>
2022-10-08 14:02:42 +0300

    Update

--- Assets/CalcScript.cs
+++ Assets/CalcScript.cs
@@ -4,8 +4,21 @@ using UnityEngine.UI;
 public class CalcScript : MonoBehaviour{
 	[SerializeField] GameObject inputField;
 	[SerializeField] GameObject textDisplay;
+	
+	int temp;
 
 	void Update(){
-		textDisplay.GetComponent<Text>().text = ((1.58781 * int.Parse(inputField.GetComponent<Text>().text)) + 47.4405).ToString("F2");
+		int number;
+		bool sucsess = int.TryParse(inputField.GetComponent<Text>().text, out number);
+		
+		if(sucsess){
+			temp = number;
+		}
+		
+		if(temp > 0){
+			textDisplay.GetComponent<Text>().text = ((1.58781 * temp) + 47.4405).ToString("F2");
+		}else{
+			textDisplay.GetComponent<Text>().text = "NaN";
+		}
 	}
 }
